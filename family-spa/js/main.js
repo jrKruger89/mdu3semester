7bbbf067a3943094bc5bc64dca82abdfb2e7f877
jrKruger89 <35734267+jrKruger89@users.noreply.github.com>
2021-09-12 13:42:07 +0200

    add functionality

---
+++ family-spa/js/main.js
@@ -0,0 +1,62 @@
+"use strict";
+
+let familyMembers = [
+	{
+		name: "Michael",
+		relation: "Father",
+		age: 61,
+		img: "",
+	},
+	{
+		name: "Susanne",
+		relation: "Mother",
+		age: 59,
+		img: "",
+	},
+	{
+		name: "Louise",
+		relation: "Sister",
+		age: 36,
+		img: "",
+	},
+];
+
+let append = (persons) => {
+	let htmlTemplate = "";
+	for (let person of persons) {
+		console.log(person);
+		htmlTemplate += `
+        <article>
+            <img src="${person.img}">
+            <h3>Name: ${person.name}</h3>
+            <p>Age: ${person.age}</p>
+            <p>Relation: ${person.relation}</p>            
+        </article>
+        `;
+	}
+	document.querySelector("#family-grid").innerHTML = htmlTemplate;
+};
+
+append(familyMembers);
+
+let add = () => {
+	console.log("Add button clicked");
+
+	let form = document.querySelector("#form-add-family");
+
+	let inputName = document.querySelector("#name").value;
+	let inputRelation = document.querySelector("#relation").value;
+	let inputAge = document.querySelector("#age").value;
+	let inputImg = document.querySelector("#imgUrl").value;
+
+	let newFamilyMember = {
+		name: inputName,
+		relation: inputRelation,
+		age: inputAge,
+		img: inputImg,
+	};
+
+	familyMembers.push(newFamilyMember);
+	append(familyMembers);
+	form.reset();
+};
