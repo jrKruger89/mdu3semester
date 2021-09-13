9a836007540ca3b9f65c65bcf59e4a548e709db2
jrKruger89 <35734267+jrKruger89@users.noreply.github.com>
2021-09-13 13:40:43 +0200

    little bit of inheritance

--- person-class/js/main.js
+++ person-class/js/main.js
@@ -26,23 +26,56 @@ class Person {
 
     appendPerson() {
         const template = `
-      <article>
-        <h2>${this.name}</h2>
-        <a href="mailto:${this.mail}">${this.mail}</a>
-        <p>${this.getAge()}</p>
-        <img src="${this.img}">
-      </article
-      `;
+        <article>
+            <h2>${this.name}</h2>
+            <a href="mailto:${this.mail}">${this.mail}</a>
+            <p>${this.getAge()}</p>
+            <img src="${this.img}">
+        </article>
+        `;
 
         return template;
     }
 }
 
-let person1 = new Person(
+class Teacher extends Person {
+    constructor(name, mail, birthDate, img, subject, position) {
+        super(name, mail, birthDate, img);
+
+        this.subject = subject;
+        this.position = position;
+    }
+    appendPerson() {
+        const template = `
+        <article>
+            <h2>${this.name}</h2>
+            <a href="mailto:${this.mail}">${this.mail}</a>
+            <p>${this.getAge()}</p>
+            <p>${this.subject}</p>
+            <p>${this.position}</p>
+            <img src="${this.img}">
+        </article>
+        `;
+
+        return template;
+    }
+}
+
+const person1 = new Person(
     "Johnny",
     "johnny@live.dk",
     "1951-03-31",
     "https://www.mhe.dk/wp-content/uploads/2020/10/W2-Johnny-Madsen.jpg"
 );
 
+const teacher1 = new Teacher(
+    "Rasmus",
+    "race@eaaa.dk",
+    "1990-05-18",
+    "https://www.eaaa.dk/media/devlvvgj/rasmus-cederdorff.jpg?anchor=center&mode=crop&width=800&height=450&rnd=132301335324630000&format=webp",
+    "Programming",
+    "Lecturer"
+);
+
 document.querySelector("#content").innerHTML += person1.appendPerson();
+document.querySelector("#content").innerHTML += teacher1.appendPerson();
